2a6848ea6b3a3fe009d4c2b49f017ad9fc019bdc
gard971 <32454699+gard971@users.noreply.github.com>
2020-09-17 13:25:32 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var token = "NjY1NTMwMDQ3NTExMTM0MjE5.Xhm9KA.xGxoygQLOVRcNKnMX49GX2-eB_E"
+var prefix = "!"
+
+const Discord = require("discord.js")
+const client = new Discord.Client()
+
+client.login(token)
+client.on("ready", () => {
+    console.log("ready!")
+})
+
+client.on("message", (msg) => {
+    
+    var prefixFromCommand = msg.content.substring(0, prefix.length)
+    if(prefix == prefixFromCommand){
+        var command = msg.content.split(" ")[0].substring(prefix.length)
+        var args = msg.content.split(" ").splice(1)
+        switch(command.toLowerCase()){
+            case "plan":
+                msg.channel.send("https://romres.ist-asp.com/WebUntis/?school=Arstad#Timetable?type=1&id=433&formatId=1")
+                break;
+            case "its":
+                msg.channel.send("https://vlfk.itslearning.com/index.aspx")
+        }
+    }
+})
